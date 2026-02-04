fd4ad1e98fdd54196395a1591968f22e5368cc7e
WolverineAryan <36thoraryan@gmail.com>
2026-02-04 21:19:36 +0530

    UI setup ready

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { DataProvider } from "./context/DataContext.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
